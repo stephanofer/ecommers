7f8de37a9cc4d8b9a9f5a02a43585d451693f6b5
stephanofer <120071974+stephanofer@users.noreply.github.com>
2024-10-12 23:33:20 -0500

    Add: Open Graph protocol meta HTML

--- astrofront/src/lib/queries.ts
+++ astrofront/src/lib/queries.ts
@@ -1,45 +1,49 @@
 import fetchApi from "./strapi";
-import type { Favicon, HomeSEO } from "./types";
+import type { FetchHomeSEO } from "./types";
 import {STRAPI_URL} from 'astro:env/server'
 
-
-export async function HomePageSEO(): Promise<HomeSEO> {
-  const seoData = await fetchApi<{seo: HomeSEO} & {favicon: Favicon}>({
+export async function HomePageSEO(): Promise<FetchHomeSEO> {
+  const seoData = await fetchApi<FetchHomeSEO>({
     endpoint: "home",
     wrappedByKey: "data",
     query: {
       "fields[0]": "id",
       "fields[1]": "documentId",
       "populate[seo][populate][metaImage][fields][0]": "url",
+      "populate[seo][populate][metaSocial][populate][image][fields][0]": "url",
       "populate[favicon][fields][0]": "url",
     },
   });
 
+  // console.log(seoData);
+
 
   if (seoData){
-
-    
-
-
     return {
-        metaTitle: seoData.seo.metaTitle,
-        metaDescription: seoData.seo.metaDescription,
-        keywords: seoData.seo.keywords.split('\n').join(', '),
-        metaImage: {
-            url: `${STRAPI_URL}${seoData.seo.metaImage.url}`
+        seo: {
+          metaTitle: seoData.seo.metaTitle,
+          metaDescription: seoData.seo.metaDescription,
+          keywords: seoData.seo.keywords.split('\n').join(', '),
+          metaImage: {
+              url: `${STRAPI_URL}${seoData.seo.metaImage.url}`
+          },
+          metaSocial: seoData.seo.metaSocial,
         },
         favicon: {
-            url:  `${STRAPI_URL}${seoData.favicon.url}`
+            url: `${STRAPI_URL}${seoData.favicon.url}`
         }
       }
   }
 
   return {
-    metaTitle: "Tienda Virtual",
-    metaDescription: "Tienda Virtual",
-    keywords: "",
-    metaImage: {
-        url: ""
+    seo: {
+      metaTitle: "Tienda Virtual",
+      metaDescription: "Tienda Virtual",
+      keywords: "",
+      metaImage: {
+          url: ""
+      },
+      metaSocial: []
     },
     favicon: {
         url:  ""
